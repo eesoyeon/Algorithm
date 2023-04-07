d1be7228148c1e63400efbd2549a433e6d32e78d
Lee So Yeon <102416278+eesoyeon@users.noreply.github.com>
2023-04-07 23:29:26 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+ 
+    //jaron noraj
+    var answer = [...my_string].reverse().join("");
+    //answer = my_string.split('').reverse().join('');
+    
+    return answer;
+}
