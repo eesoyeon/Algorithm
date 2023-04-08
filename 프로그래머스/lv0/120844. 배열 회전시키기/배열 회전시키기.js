9e4f448a9902d01168c725460ef1bef3492830c0
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2023-04-08 18:26:13 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,18 +1,27 @@
 function solution(numbers, direction) {
     //right 0 1 2 -> 2 0 1
     //left 0 1 2 -> 1 2 0
+    // var answer = [];
+    // var leng = numbers.length;
+    // for (let i=0; i<leng-1; i++){
+    //     if(direction === 'right'){
+    //         answer[i+1] = numbers[i];
+    //         answer[0] = numbers[leng-1];
+    //     }
+    //     else {
+    //         answer[i] = numbers[i+1];
+    //         answer[leng-1] = numbers[0];
+    //     }
+    // }
     var answer = [];
-    var leng = numbers.length;
-    for (let i=0; i<leng-1; i++){
-        if(direction === 'right'){
-            answer[i+1] = numbers[i];
-            answer[0] = numbers[leng-1];
-        }
-        else {
-            answer[i] = numbers[i+1];
-            answer[leng-1] = numbers[0];
-        }
+    
+    if (direction == 'right'){
+        numbers.unshift(numbers.pop());
+    } else {
+        numbers.push(numbers.shift());
     }
+    
+    answer = numbers;
     
     return answer;
 }
