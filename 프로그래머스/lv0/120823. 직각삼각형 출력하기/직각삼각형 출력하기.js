ba7250140ea90ec847a8c4e215bee2b0fc7e3c6a
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2023-09-01 19:37:25 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 45.76 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    for(let i=1; i<=input; i++)
+        console.log('*'.repeat(i));
+});
