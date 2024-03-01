bce26e14b2f99da8208548e70e9f4aa039dd3d71
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-03-01 18:22:45 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -8,8 +8,12 @@ def solution(cards1, cards2, goal):
         if i<l1 and word==cards1[i]:
             answer.append(cards1[i])
             i += 1
+            print('i=', i) 
         if j<l2 and word==cards2[j]:
             answer.append(cards2[j])
             j += 1
+            print('j=', j)
+    
+    print(answer)
     
     return "Yes" if answer==goal else "No"
