e0680e358a5cede0ee2d23bdbf7da30c511b09fe
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-05-22 17:49:50 +0900

    [level 1] Title: 하샤드 수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,6 +1,11 @@
 def solution(x):
-    li = list(map(int, str(x)))
-    if x % sum(li) == 0:
+    sum_val = 0
+    a = x
+    while a > 0:
+        sum_val += a%10
+        a //= 10
+        
+    if x % sum_val == 0:
         return True
     else:
         return False
