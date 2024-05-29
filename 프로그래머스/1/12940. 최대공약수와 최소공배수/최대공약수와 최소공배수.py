273a46109055917e36768695acf022f971d61739
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-05-29 19:25:15 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,14 +1,8 @@
 def solution(n, m):
-    a = [i for i in range(1, n+1) if n%i==0]
-    b = [i for i in range(1, m+1) if m%i==0]
-    
-    common_values = [i for i in a if i in b]
-    
-    if len(common_values) == 1:
-        gcd = 1
-        lcm = n*m
-    else:
-        gcd = max(common_values)
-        lcm = n*m//gcd
-    
-    return gcd, lcm
+    c, d = max(n, m), min(n, m)
+    t = 1
+    while t>0:
+        t = c%d
+        c, d = d, t
+    answer = [c, int(n*m/c)]
+    return answer
