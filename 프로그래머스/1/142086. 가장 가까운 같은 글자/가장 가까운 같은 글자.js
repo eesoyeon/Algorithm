7862e2cfe134eb034db4931bd18b8a27ba73c3b5
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-06-08 20:36:12 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.54 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    var answer = [];
+    
+    var last = {};
+    for(let i=0; i<s.length; i++){
+        let char = s[i];
+        
+        if(last[char] === undefined) answer.push(-1);
+        else answer.push(i-last[char]);
+        
+        last[char] = i;
+    }
+    
+    return answer;
+}
