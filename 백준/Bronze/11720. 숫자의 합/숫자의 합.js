0373caca4ca40824a439781ea33021ff945932ba
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-09-21 19:57:16 +0900

    [Bronze IV] Title: 숫자의 합, Time: 96 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.js
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -3,10 +3,11 @@ const filePath = process.platform === 'linux' ? 'dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const N = parseInt(input[0]);
+let numbers = input[1].split('');
 
 let sum = 0;
 for (let i = 0; i < N; i++) {
-    sum += parseInt(input[1][i]);
+    sum += parseInt(numbers[i]);
 }
 
 console.log(sum);
