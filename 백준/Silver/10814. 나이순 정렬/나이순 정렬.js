27b435fad06610e4568a085f17b535d3ba00dd53
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2024-10-30 03:40:46 +0900

    [Silver V] Title: 나이순 정렬, Time: 372 ms, Memory: 48960 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -5,6 +5,11 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const N = parseInt(input[0]);
 const users = input.slice(1, N + 1).map((e) => e.split(' '));
 
-const sortedUsers = users.sort((a, b) => a[0] - b[0]);
+let answer = '';
+const sortedUsers = users
+    .sort((a, b) => a[0] - b[0])
+    .forEach((item) => {
+        answer += item.join(' ') + '\n';
+    });
 
-sortedUsers.forEach((v) => console.log(v.join(' ')));
+console.log(answer.trim());
