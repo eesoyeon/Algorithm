d94f728f49ad0130987f8e3d3d234020da9ee945
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2025-03-17 18:06:47 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 28.45 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,20 +1,26 @@
 function solution(bridge_length, weight, truck_weights) {
     let time = 0;
     let queue = new Array(bridge_length).fill(0); // 다리 상태
+    let currentWeight = 0;
     
-    
-    while(truck_weights.length || queue.some(v => v >0)) {
+    while(truck_weights.length > 0) {
         time++;
-        queue.shift();
+        currentWeight -= queue.shift();
         
-        let sum = queue.reduce((acc, cur) => acc + cur, 0);
-        if(truck_weights.length > 0 && sum + truck_weights[0] <= weight) {
-            queue.push(truck_weights.shift());
+        if(currentWeight + truck_weights[0] <= weight) {
+            let newTruck = truck_weights.shift();
+            queue.push(newTruck);
+            currentWeight += newTruck;
         } else {
             queue.push(0);
         }
     }
     
+    while(currentWeight > 0) {
+        time++;
+        currentWeight -= queue.shift();
+        queue.push(0);
+    }
     
     return time;
 }
