65e16c77a7ec2cd257d931df864d59c290c593ce
Soyeon Lee <102416278+eesoyeon@users.noreply.github.com>
2025-03-24 22:35:47 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 39.01 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 37.9 MB, 시간: 28.45 ms
+메모리: 37.9 MB, 시간: 39.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 18:06:45
+2025년 03월 24일 22:35:44
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,12 +1,15 @@
 function solution(bridge_length, weight, truck_weights) {
     let time = 0;
-    let queue = new Array(bridge_length).fill(0); // 다리 상태
-    let currentWeight = 0;
+    let queue = new Array(bridge_length).fill(0); // 다리 상태 나타내는 큐
+    let currentWeight = 0; // 현재 다리 위 무게
     
     while(truck_weights.length > 0) {
         time++;
+        
+        // 다리에서 트럭 이동
         currentWeight -= queue.shift();
         
+        // 새 트럭 올릴 수 있으면, 그 무게 빼서 queue에 올리기
         if(currentWeight + truck_weights[0] <= weight) {
             let newTruck = truck_weights.shift();
             queue.push(newTruck);
@@ -16,11 +19,6 @@ function solution(bridge_length, weight, truck_weights) {
         }
     }
     
-    while(currentWeight > 0) {
-        time++;
-        currentWeight -= queue.shift();
-        queue.push(0);
-    }
-    
-    return time;
+  
+    return time + bridge_length;
 }
